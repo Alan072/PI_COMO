6f8a1efc1895ed1b23541e170fa6331db53f5cee
Alan072 <91845623+Alan072@users.noreply.github.com>
2023-03-29 17:06:22 -0600

    migraciones completas

--- database/migrations/2023_03_12_161253_tabla_producto.php
+++ database/migrations/2023_03_12_161253_tabla_producto.php
@@ -11,13 +11,80 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
-        Schema::create('producto', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('nombre');
-            $table->string('descripcion');
+        Schema::create('ubicacion', function (Blueprint $table) {
+            $table->increments('id_ubicacion');
+            $table->string('pasillo');
+            $table->integer('racks');
             $table->timestamps();
         });
+        
+        Schema::create('rol_desempena', function (Blueprint $table) {
+            $table->increments('id_rol');
+            $table->string('nombre_rol');
+            $table->timestamps();
+        });
+        
+        Schema::create('producto', function (Blueprint $table) {
+            $table->increments('id_producto');
+            $table->string('nombre_producto');
+            $table->text('descripcion');
+            $table->timestamps();
+        });
+        
+        Schema::create('almacen_producto', function (Blueprint $table) {
+            $table->increments('id_almacen');
+            $table->integer('stock');
+            $table->float('precio');
+            $table->unsignedInteger('producto_id');
+            $table->unsignedInteger('ubicacion_id');
+            $table->foreign('producto_id')->references('id_producto')->on('producto')->onDelete('cascade');
+            $table->foreign('ubicacion_id')->references('id_ubicacion')->on('ubicacion')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+        Schema::create('entrada', function (Blueprint $table) {
+            $table->increments('id_entrada');
+            $table->unsignedInteger('producto_id');
+            $table->foreign('producto_id')->references('id_producto')->on('producto')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+        Schema::create('tipo_usuario', function (Blueprint $table) {
+            $table->increments('id_usuario');
+            $table->string('nombre');
+            $table->string('empresa');
+            $table->string('direccion');
+            $table->string('pais');
+            $table->string('correo');
+            $table->string('telefono_celular');
+            $table->string('telefono_fijo');
+            $table->unsignedInteger('rol_desempena');
+            $table->foreign('rol_desempena')->references('id_rol')->on('rol_desempena')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+        Schema::create('salida', function (Blueprint $table) {
+            $table->increments('id_salida');
+            $table->unsignedInteger('producto_id');
+            $table->unsignedInteger('usuario_id');
+            $table->foreign('producto_id')->references('id_producto')->on('producto')->onDelete('cascade');
+            $table->foreign('usuario_id')->references('id_usuario')->on('tipo_usuario')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+        Schema::create('tarea', function (Blueprint $table) {
+            $table->increments('id_tarea');
+            $table->text('descripcion');
+            $table->unsignedInteger('salida_id');
+            $table->unsignedInteger('ubicacion_id');
+            $table->unsignedInteger('entrada_id');
+            $table->foreign('salida_id')->references('id_salida')->on('salida')->onDelete('cascade');
+            $table->foreign('ubicacion_id')->references('id_ubicacion')->on('ubicacion')->onDelete('cascade');
+            $table->foreign('entrada_id')->references('id_entrada')->on('entrada')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+
     }
 
     /**
